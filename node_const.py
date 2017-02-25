271b731dc65098c980025149b24acab09b1f297f
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2017-02-25 15:37:50 +0700

    Add ops and summary writer

--- node_const.py
+++ node_const.py
@@ -6,9 +6,17 @@ if __name__ == '__main__':
     node2 = tf.constant(4.0)
 
     # graph is not evaluated yet
-    print(node1, node2)
+    print('node1:', node1)
+    print('node2:', node2)
+
+    # Add ops to the graph
+    sumnode = tf.add(node1, node2)
+    print('sumnode:', sumnode)
 
     # run the graph to evaluate the result
     sess = tf.Session()
-    result = sess.run([node1, node2])
+    result = sess.run([sumnode])
     print(result)
+
+    # Summary Writer
+    summary_writer = tf.summary.FileWriter('logs', sess.graph)
